7cba8568f903ab3bbb7ea97438e07139bce7b5dd
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-11 21:38:21 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# My Each
+hey # My Each
 
 ## Objectives
 
--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+def my_each(collection) #do  # |i|
+if 
+  i = 0 
+  while i < collection.length 
+     yield (collection[i])
+    puts i 
+  end
+
+end 
+collection
+end 
